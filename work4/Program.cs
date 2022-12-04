f383ae02877d19bcecd570c76f7242c9d441107a
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-04 19:47:45 +0300

    work4

---
+++ work4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число1:");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число2:");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число3:");
+int c=Convert.ToInt32(Console.ReadLine());
+ if (a>b)
+Console.WriteLine($"max:{a}");
+ else if (b>c)
+Console.WriteLine($"max:{b}");
+ else if (c>a)
+Console.WriteLine($"max:{c}");
