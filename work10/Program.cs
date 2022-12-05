e61a33d9d9e5cd70e40a5b8f0e6b5deae7a825fe
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-05 10:32:13 +0300

    задача 10

---
+++ work10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Трехзначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+	Console.Write("Вы ошиблись!\nТрехзначное число:");
+	n = Convert.ToInt32(Console.ReadLine());
+}
+string n2 = Convert.ToString(n);
+Console.WriteLine("вторая цифра этого числа " + n2[1]);
