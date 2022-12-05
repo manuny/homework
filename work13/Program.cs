e77894d25df38b57901324d4ba7e485ea9d03876
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-05 11:14:00 +0300

    задача 13

---
+++ work13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Число:");
+int n = Convert.ToInt32(Console.ReadLine());
+string t = Convert.ToString(n);
+if (t.Length > 2)
+{
+	Console.Write(" третья цифра " + t [2]);
+}
+else
+{
+	Console.Write("третьей цифры нет");
+}
