b497fdb589bb704c72867de9e3e578d163e36d3f
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-05 11:21:43 +0300

    задача 15

---
+++ work15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите день недели:");
+int n=Convert.ToInt32(Console.ReadLine());
+ while (n>8 || n<1)
+ {
+Console.Write("Вы ошиблись!\nВведите день недели:");
+n=Convert.ToInt32(Console.ReadLine());
+ }
+ if (n==6 || n==7)
+  Console.WriteLine("Да");
+ else 
+  Console.WriteLine("Нет");
