983a2e01b3fa01cc0c18f52f8958329b3dcfde22
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-08 14:41:22 +0300

    доп зад

---
+++ dop zad/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите числа:");
+int n = Convert.ToInt32(Console.ReadLine());
+	int max1 = 0;
+	int max2 = 0;
+	while (n != 0)
+{
+    if (n > max1)
+    {
+        max2 = max1;
+        max1 = n;
+    }
+    else if (n > max2)
+    {
+        max2 = n;
+    }
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Значение второго максимального: {max2}");
