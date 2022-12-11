64dccea7744dc96fb48fc735b1c93a2da6bd9cb9
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-12 01:17:20 +0300

    work19

---
+++ work19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("пятизначное число:");
+string? num = Console.ReadLine();
+while (num!.Length != 5)
+{
+  Console.WriteLine($"Введи правильное число");
+  num = Console.ReadLine();
+}
+
+  if (num[0]==num[4] || num[1]==num[3])
+  {
+    Console.WriteLine($"Ваше число: {num} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {num} - НЕ палиндром.");
+
