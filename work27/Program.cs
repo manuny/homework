91739c90ee46808c393357cf552ef1c26cd3e19c
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-14 21:36:17 +0300

    Work27

---
+++ work27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write ("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int Conv = Convert.ToString(A).Length;
+int b = 0;
+int result = 0;
+for (int i = 0; i < Conv; i++)
+{
+      b = A - A % 10;
+      result = result + (A - b);
+      A = A / 10;
+}
+Console.WriteLine("Ответ: " + result);
