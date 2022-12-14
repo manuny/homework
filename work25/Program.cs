a0373609776a474158f31857b1937eef70afa3f3
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-14 21:08:29 +0300

    Work25

---
+++ work25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write ("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine()), result = A;
+for (int i = 2; i <= B; i++)
+{
+result = A * result;
+}
+
+Console.WriteLine("Ответ: " + result);
