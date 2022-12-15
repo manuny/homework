5fda7f547c88fe5d2e02ee670819080b5d01ea21
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-15 14:26:48 +0300

    Work29

---
+++ work29/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write ("Введите количество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+
+for (int i = 0; i < array.Length; i++)
+{
+Console.WriteLine ("Введите элементы массива: ");
+array [i] = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine($"[{string.Join (", ", array)}]");
