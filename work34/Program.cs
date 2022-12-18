5c596e4197122103192ab586bc6522fca7c99b9b
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-18 21:24:08 +0300

    work34

---
+++ work34/Program.cs
@@ -0,0 +1,22 @@
+﻿void InputArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+		array[i] = new Random().Next(100, 999);
+}
+int ReleaseArray(int[] array, int k)
+{
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] % 2 == 0)
+			k = ++k;
+	}
+	return k;
+}
+Console.Clear();
+int[] array = new int[100];
+InputArray(array);
+Console.WriteLine($" массив [{string.Join(" , ", array)}]");
+int k = 0;
+ReleaseArray(array, k);
+Console.WriteLine("Количество четных чисел: " + ReleaseArray(array, k));
