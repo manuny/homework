e65364591dfc819790be2bfaf1199872ea68b3be
Vasileva Masha <Vasilevamasha1982@gmail.com>
2022-12-23 21:46:56 +0300

    work41

---
+++ work41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите числа через пробел:");
+int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int a = 0;
+for (int i = 0; i < s.Length; i++)
+{
+	if (s[i] > 0)
+		a = a + 1;
+}
+
+Console.WriteLine(a + $" -  чисел > 0 ");
