a7ca4a513637a4db4e3c24dc3713b2e32e6794ae
Vasileva Masha <Vasilevamasha1982@gmail.com>
2023-01-12 02:37:33 +0300

    work66

---
+++ work66/Program.cs
@@ -0,0 +1,15 @@
+﻿int rec(int n, int m)
+{
+    if (n == m)
+        return n;
+    return rec(n, m - 1) + m;
+
+}
+
+
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
